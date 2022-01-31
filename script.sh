2665408b6bccc3964ee036c227df30795d0653a7
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2022-01-31 16:26:06 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@ while true
 do 
 	filename=$(echo $RANDOM)
 	touch $filename
-	cat /dev/urandom | tr -cd 'a-f0-9' | head -c 20000 >> /usr/share/nginx/html/nfs_data/$filename
+	cat /dev/urandom | tr -cd 'a-f0-9' | head -c 20000 >> /usr/share/nginx/html/$filename
 done
